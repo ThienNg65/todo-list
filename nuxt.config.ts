9eb5c28bccf2728fd88d82322ae50a8f3a27e327
khanh.lecong <khanh.lecong>
2025-11-08 21:34:47 +0700

    Fix: Remove duplicate srcDir declaration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   srcDir: './app',
-  srcDir: './app',
 
   modules: ['@nuxtjs/tailwindcss'],
 })
